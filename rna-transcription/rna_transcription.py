290906cb99651a839ecfbd6c720ea3d93eb2f0eb
Neex-IO <neex-io@users.noreply.github.com>
2017-05-06 16:25:22 -0700

    Add RNA transcription.

---
+++ rna-transcription/rna_transcription.py
@@ -0,0 +1,14 @@
+def to_rna(dna):
+    rna = ""
+    for char in dna:
+        if char == "G":
+            rna += "C"
+        elif char == "C":
+            rna += "G"
+        elif char == "T":
+            rna += "A"
+        elif char == "A":
+            rna += "U"
+        else:
+            return ""
+    return rna
